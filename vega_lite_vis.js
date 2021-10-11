10754edaa058317fbbfb85bba654acc5f6e61967
Wangjie Zhong <wzho0026@student.monash.edu>
2021-10-11 15:25:36 +1100

    new2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,10 @@
 var map = "co2_by_country_long.json"; // Looks like it can't load this file for some reason.
 var line = "co2_line_chart.json";
-var stacked = "consumption.json";
+var stackedA = "consumption.json";
+var stackedB = "consumption_by_sector.json";
 var u_production = "uranium_production_by_country.json";
 vegaEmbed("#continentLines", line).then(function(result) {}).catch(console.error);
 vegaEmbed("#map", map).then(function(result) {}).catch(console.error);
-vegaEmbed('#stacked', stacked).then(function(result) {}).catch(console.error);
+vegaEmbed('#stackedA', stackedA).then(function(result) {}).catch(console.error);
+vegaEmbed('#stackedB', stackedB).then(function(result) {}).catch(console.error);
 vegaEmbed('#u_production', u_production).then(function(result) {}).catch(console.error);
