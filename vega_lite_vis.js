b79cb7d537d897feb265e976d399320c446882d5
Wangjie Zhong <wzho0026@student.monash.edu>
2021-10-13 16:55:11 +1100

    updated

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,8 +3,13 @@ var line = "co2_line_chart.json";
 var stackedA = "consumption.json";
 var stackedB = "consumption_by_sector.json";
 var u_production = "uranium_production_by_country.json";
+var energy_density_s = "energy_content.json";
+var energy_density = "energy_content_bubble.json";
+
 vegaEmbed("#continentLines", line).then(function(result) {}).catch(console.error);
 vegaEmbed("#map", map).then(function(result) {}).catch(console.error);
 vegaEmbed('#stackedA', stackedA).then(function(result) {}).catch(console.error);
 vegaEmbed('#stackedB', stackedB).then(function(result) {}).catch(console.error);
 vegaEmbed('#u_production', u_production).then(function(result) {}).catch(console.error);
+vegaEmbed('#slope', energy_density_s).then(function(result) {}).catch(console.error);
+vegaEmbed('#bubble', energy_density).then(function(result) {}).catch(console.error);
